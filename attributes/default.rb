a173a5d1e34d3e133eb6864de558e428dfa37650
Jack Viers <jackcviers@gmail.com>
2013-12-12 16:20:11 -0600

    Add correct attributes to install java
    https://trello.com/c/sjQRJndp/12-artifactory-cm

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+include_attribute 'java'
+
+default['java']['install_flavor'] = 'openjdk'
+default['java']['openjdk_packages'] = ['openjdk-7-jre', 'openjdk-7-jdk']
+default['java']['jdk_version'] = '7'
