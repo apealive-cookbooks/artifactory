3ad48904cbafe5e6dd3d218a718dc2628ac3e260
Jack Viers <jackcviers@gmail.com>
2013-12-12 15:36:18 -0600

    Expect java::default inclusion
    https://trello.com/c/sjQRJndp/12-artifactory-cm

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -6,7 +6,11 @@ describe 'artifactory' do
     end.converge("#{described_cookbook}")
   end
 
-  it 'should include the apt cookbook' do
+  it 'should include the apt recipe' do
     expect(chef_run).to include_recipe 'apt'
+  end
+
+  it 'should include the java recipe' do
+    expect(chef_run).to include_recipe 'java'
   end
 end
