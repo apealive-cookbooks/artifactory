e0b8df5e711397339e180836814b97083f8bd567
Jack Viers <jackcviers@gmail.com>
2013-12-12 15:32:33 -0600

    Expect artifactory::default to include recipe
    apt::default
    
    https://trello.com/c/sjQRJndp/12-artifactory-cm

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -3,7 +3,7 @@ require_relative 'spec_helper'
 describe 'artifactory' do
   let(:chef_run) do
     ChefSpec::Runner.new do |node|
-    end.converge('described_cookbook')
+    end.converge("#{described_cookbook}")
   end
 
   it 'should include the apt cookbook' do
