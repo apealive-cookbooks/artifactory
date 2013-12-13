2c08240d625dee2c43697ff7ccca2806663e3e36
Jack Viers <jackcviers@gmail.com>
2013-12-13 11:31:15 -0600

    Add dependency on ark
    https://trello.com/c/sjQRJndp/12-artifactory-cm

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description      "Sets up open-source artifactory"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version "0.1.0"
 
+depends "ark"
 depends "apt"
 depends "runit"
 depends "java"
