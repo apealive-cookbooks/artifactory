6d004d42ca9e48eec06107b590a2d95e33fdda97
Jack Viers <jackcviers@gmail.com>
2013-12-18 17:42:51 -0600

    Add ulimit recipe
    https://trello.com/c/sjQRJndp/12-artifactory-cm

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      "Sets up open-source artifactory"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version "0.1.0"
 
+depends "ulimit", "~> 0.3"
 depends "ark"
 depends "apt"
-depends "runit"
 depends "java", "~> 1.16"
