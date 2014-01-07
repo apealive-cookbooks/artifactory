fe00ff428dbe56fe402fce6f0b9a32d411469f8b
Luke Amdor <luke.amdor@banno.com>
2014-01-07 14:43:49 -0600

    cleaning up serverspecs

--- test/integration/artifactory/serverspec/default_spec.rb
+++ test/integration/artifactory/serverspec/default_spec.rb
@@ -1,24 +1,10 @@
-require 'serverspec'
-require 'io/console'
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
+require 'spec_helper'
 
-RSpec.configure do |c|
-  c.before :all do
-    c.path = '/sbin:/usr/sbin'
-  end
+describe service("artifactory") do
+  it { should be_running }
 end
 
-describe 'artifactory' do
-  it 'should install artifactory' do
-    expect(file('/opt/artifactory/bin/artifactoryctl')).to be_file
-  end
-
-  it 'should start the artifactory web service on port 8081' do
-    cmd = IO.popen("netstat -nlp")
-    res = cmd.readlines
-    cmd.close
-    expect(res.any? {|str| str.include? "8081" }).to be_true
-  end
+describe port(8081) do
+  it { should be_listening }
 end
 
