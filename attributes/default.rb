a9715689d92b486e10fd337f924669e0beb66186
Luke Amdor <luke.amdor@banno.com>
2014-01-08 09:08:00 -0600

    updating to artifactory 3.1.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ default['java']['install_flavor'] = 'openjdk'
 default['java']['openjdk_packages'] = ['openjdk-7-jre-headless', 'openjdk-7-jdk']
 default['java']['jdk_version'] = '7'
 
-default['artifactory']['version'] = '3.0.4'
+default['artifactory']['version'] = '3.1.0'
 default['artifactory']['user'] = 'artifactory'
 default['artifactory']['dir'] = '/opt/artifactory'
 default['artifactory']['java_options'] = '-server -Xms512m -Xmx2g -Xss256k -XX:PermSize=128m -XX:MaxPermSize=128m -XX:+UseG1GC'
